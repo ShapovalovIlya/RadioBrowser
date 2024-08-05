0abbb7d0c294c2456fedce1462ff4ed7a236ef8d
Ilia Shapovalov <greateman@mail.ru>
2024-08-05 22:51:48 +0300

    extend documentation

--- Sources/RadioBrowser/RadioBrowser.swift
+++ Sources/RadioBrowser/RadioBrowser.swift
@@ -5,6 +5,7 @@ import Foundation
 import SwiftFP
 import OSLog
 
+/// Основной класс для работы с `API all.api.radio-browser`
 public final class RadioBrowser: Sendable {
     
     /// Публичный экземпляр `RadioBrowser` с конфигурацией по умолчанию.
@@ -17,6 +18,11 @@ public final class RadioBrowser: Sendable {
     private let logger: Logger?
     
     //MARK: - init(_:)
+    
+    /// Возвращает экземпляр с использованием пользовательских настроек.
+    /// - Parameters:
+    ///   - config: экземпляр конфигурации сессии
+    ///   - logger: экземпляр логгера
     public init(
         config: URLSessionConfiguration = .default,
         logger: Logger? = nil
@@ -39,57 +45,68 @@ public final class RadioBrowser: Sendable {
     /// - Parameters:
     ///   - offset: отступ. Для пагинации.
     ///   - limit: максимальный размер массива элементов в запросе.
-    /// - Returns: Результат запроса или ошибка, возникшая в процессе.
+    /// - Returns: коллекция ``StationTag`` или ошибка ``RadioError``, возникшая в процессе.
     @Sendable
     public func getTags(
         offset: Int = 0,
         limit: Int = 20
     ) async -> Result<[StationTag], RadioError> {
-        await perform(.get, .tags(offset: offset, limit: limit))
-            .decodeJSON([StationTag].self, decoder: decoder)
-            .mapError(RadioError.map(_:))
+        await perform(
+            .get,
+            .tags(offset: offset, limit: limit),
+            ofType: [StationTag].self
+        )
     }
     
     /// Возвращает cписок всех стран радио станций.
     /// - Parameters:
     ///   - offset: отступ. Для пагинации.
     ///   - limit: максимальный размер массива элементов в запросе.
+    /// - Returns: коллекция ``Country`` или ошибка ``RadioError``, возникшая в процессе.
     @Sendable
     public func getCountries(
         offset: Int = 0,
         limit: Int = 20
     ) async -> Result<[Country], RadioError> {
-        await perform(.get, .countries(offset: offset, limit: limit))
-            .decodeJSON([Country].self, decoder: decoder)
-            .mapError(RadioError.map(_:))
+        await perform(
+            .get,
+            .countries(offset: offset, limit: limit),
+            ofType: [Country].self
+        )
     }
     
     /// Возвращает cписок всех радио станций
     /// - Parameters:
     ///   - offset: отступ. Для пагинации.
     ///   - limit: максимальный размер массива элементов в запросе.
+    /// - Returns: коллекция ``Station`` или ошибка ``RadioError``, возникшая в процессе.
     @Sendable
     public func getAllStations(
         offset: Int = 0,
         limit: Int = 20
     ) async -> Result<[Station], RadioError> {
-        await perform(.get, .all(offset: offset, limit: limit))
-            .decodeJSON([Station].self, decoder: decoder)
-            .mapError(RadioError.map(_:))
+        await perform(
+            .get,
+            .all(offset: offset, limit: limit),
+            ofType: [Station].self
+        )
     }
     
     /// Возвращает cписок радио-станций с самым высоким рейтингом
     /// - Parameters:
     ///   - offset: отступ. Для пагинации.
     ///   - limit: максимальный размер массива элементов в запросе.
+    /// - Returns: коллекция ``Station`` или ошибка ``RadioError``, возникшая в процессе.
     @Sendable
     public func getPopularStation(
         offset: Int = 0,
         limit: Int = 20
     ) async -> Result<[Station], RadioError> {
-        await perform(.get, .topVote(offset: offset, limit: limit))
-            .decodeJSON([Station].self, decoder: decoder)
-            .mapError(RadioError.map(_:))
+        await perform(
+            .get,
+            .topVote(offset: offset, limit: limit),
+            ofType: [Station].self
+        )
     }
     
     /// Возвращает cписок радио-станций, чью название совпадает/ содержит передаваемую строку
@@ -97,15 +114,18 @@ public final class RadioBrowser: Sendable {
     ///   - name: название радио-станции
     ///   - offset: отступ. Для пагинации.
     ///   - limit: максимальный размер массива элементов в запросе.
+    /// - Returns: коллекция ``Station`` или ошибка ``RadioError``, возникшая в процессе.
     @Sendable
     public func searchStation(
         named name: String,
         offset: Int = 0,
         limit: Int = 20
     ) async -> Result<[Station], RadioError> {
-        await perform(.get, .search(byName: name, offset: offset, limit: limit))
-            .decodeJSON([Station].self, decoder: decoder)
-            .mapError(RadioError.map(_:))
+        await perform(
+            .get,
+            .search(byName: name, offset: offset, limit: limit),
+            ofType: [Station].self
+        )
     }
     
     /// Возвращает cписок радио-станций, ассоциированных с переданными идентификаторами.
@@ -115,22 +135,27 @@ public final class RadioBrowser: Sendable {
     ///  - Сервис отдаст столько станций, сколько передано `UUID` в метод
     ///
     /// - Parameter uuids: массив уникальных идентификаторов радио-станций
-    /// - Returns: Массив с радио станциями, согласно переданным `UUID`, либо ошибка, возникшая в процессе запроса
+    /// - Returns: коллекция ``Station`` или ошибка ``RadioError``, возникшая в процессе.
     @Sendable
     public func getStations(withIds uuids: [UUID]) async -> Result<[Station], RadioError> {
-        await perform(.get, .stations(withIds: uuids))
-            .decodeJSON([Station].self, decoder: decoder)
-            .mapError(RadioError.map(_:))
+        await perform(
+            .get,
+            .stations(withIds: uuids),
+            ofType: [Station].self
+        )
     }
     
     /// Возвращает радио-станцию по переданному `UUID`, если такая есть
     /// - Parameter id: уникальный идентификатор радио-станции
+    /// - Returns: ``Station`` или ошибка ``RadioError``, возникшая в процессе.
     @Sendable
     public func getStation(withId id: UUID) async -> Result<Station?, RadioError> {
-        await perform(.get, .stations(withIds: [id]))
-            .decodeJSON([Station].self, decoder: decoder)
-            .mapError(RadioError.map(_:))
-            .map(\.first)
+        await perform(
+            .get,
+            .stations(withIds: [id]),
+            ofType: [Station].self
+        )
+        .map(\.first)
     }
 }
 
@@ -167,15 +192,15 @@ private extension RadioBrowser {
 //MARK: - Private methods
 private extension RadioBrowser {
     
-    func perform(
+    func perform<T: Decodable>(
         _ method: RequestType,
-        _ endpoint: Endpoint
-    ) async -> Result<Data, Error> {
+        _ endpoint: Endpoint,
+        ofType type: T.Type
+    ) async -> Result<T, RadioError> {
         logger?.trace(
             """
             Performing request... 
             Method: \(String(describing: method))
-            
             Endpoint: \(String(describing: endpoint.string))
             """
         )
@@ -189,12 +214,16 @@ private extension RadioBrowser {
         }
         .asyncTryMap(session.data)
         .tryMap(unwrap(payload:))
+        .decodeJSON(type.self, decoder: decoder)
+        .mapError(RadioError.map(_:))
     }
     
     func unwrap(payload: Payload) throws -> Data {
         guard let httpResponse = payload.response as? HTTPURLResponse else {
-            logger?.error("Invalid response type: \(String(describing: payload.response))")
-            throw URLError(.cannotParseResponse)
+            throw URLError(.cannotParseResponse, userInfo: ["Invalid response": payload.response])
+        }
+        if let error = RadioError(statusCode: httpResponse.statusCode) {
+            throw error
         }
         return payload.data
     }
--- Sources/RadioBrowser/RadioError.swift
+++ Sources/RadioBrowser/RadioError.swift
@@ -7,10 +7,17 @@
 
 import Foundation
 
+/// Тип ошибок, возникающих в процессе работы ``RadioBrowser``.
 public enum RadioError: Error, Sendable {
     case decodeFail(DecodingError)
     case encodeFail(EncodingError)
     case unknown(Error)
+    
+    init?(statusCode: Int) {
+        switch statusCode {
+        default: return nil
+        }
+    }
     
     static func map(_ error: Error) -> RadioError {
         switch error {
